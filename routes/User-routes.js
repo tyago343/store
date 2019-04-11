f615a9779241240a474d6989789756d1d012ee5b
Santiago Andres Casanova <tyagocasanova@hotmail.com>
2019-04-11 22:02:44 +0200

    first routes0

--- routes/User-routes.js
+++ routes/User-routes.js
@@ -1,7 +1,26 @@
 const User = require ('../db/models/User.js');
 const router = require ('express').Router();
-
+const uuid = require ('uuid/v4');
 
 router.get('/', (req, res) => {
-    User.create()
+    User.findAll()
+    .then(response=>{
+        res.send(response.data)
+    })
+})
+router.post('/', (req, res) => {
+    const { body } = req;
+    User.create({
+        id: uuid(),
+        name: body.name,
+        lastName: body.lastName,
+        email: body.email,
+        password: body.password
+    })
+    .then(response=>{
+        res.send(response)
+    })
+})
+router.get('/:id', (req, res) => {
+    
 })
