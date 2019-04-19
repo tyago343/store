9532304dd4a7f7879c29412da4471258667b0c0b
Santiago Andres Casanova <tyagocasanova@hotmail.com>
2019-04-19 12:14:26 +0200

    formulario de login sin estilos

---
+++ src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+export default Login = (props) => {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const handleInputChange = (evt) => {
+        evt.preventDefault();
+        let field = evt.target.name;
+        let value = evt.target.value;
+        if(field == 'email'){
+            setEmail(value);
+        }else{
+            setPassword(value)
+        }
+
+    };
+    return (
+        <section>
+            <form action="" method="post">
+                <h3>Iniciar sesión</h3>
+                <div>
+                    <label htmlFor="email">Correo electrónico</label>
+                    <input type="text" name="email" value={email} onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" name="password" value={password} onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <button type="submit"></button>
+                </div>
+            </form>
+        </section>
+    )
+};
